56462da0027cc03613421fd25aa55b21b4205466
mr.b <bruno932151@gmail.com>
2022-05-15 09:56:15 -0500

    fixing missing lines

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,10 +6,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def initFlask():
     ''' print message '''
-    return “Hello HBNB!”
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
